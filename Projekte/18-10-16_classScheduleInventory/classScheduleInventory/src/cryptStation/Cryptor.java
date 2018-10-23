10a6a3f6290530f4ad0933a8a70429dada5becaa
owner <owner@breath>
2018-10-23 21:40:42 +0200

    Kommentare hinzugefügt Cryptor temporär fertiggestellt Konstanten hinzugefügt

--- Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
+++ Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
@@ -1,15 +1,31 @@
 package cryptStation;
 
 public class Cryptor {
-    public static char [] enrypt(char [] plainPassword){
-        char [] encrypted = new char [0];
-        //TODO
-        return encrypted;
+
+    public static char[] enrypt(char[] plainPassword) {
+        if (plainPassword != null) {
+            char[] encrypted = new char[0];
+            for (int i = 0; i < plainPassword.length-1; i++) {
+                encrypted[i] = (char) ((double) plainPassword[i]
+                        * Math.sin((double) plainPassword[i]));
+            }
+            return encrypted;
+        }
+
+        return new char[10];
+        
     }
-    
-    public static String decrypt(char [] encryptedPass){
-        String plain = "Password";
-        //TODO
-        return plain;
+
+    public static String decrypt(char[] encryptedPass) {
+        if (encryptedPass != null) {
+            String plain = "";
+            for (int i = 0; i < encryptedPass.length-1; i++) {
+                plain += (char) ((double) encryptedPass[i])
+                        / Math.sin((double) encryptedPass[i]);
+            }
+            return plain;
+        }
+
+        return new String("Password");
     }
 }
--- Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/gui/GUI.java
+++ Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/gui/GUI.java
@@ -16,17 +16,26 @@ import javax.swing.JOptionPane;
 
 public class GUI extends javax.swing.JFrame {
 
+    //Verwaltung der HÜs und Termine
     private ScheduleInventory schedInv;
     private AppointmentInventory appInv;
 
+    //zum Auswählen der lokalen Dateien
     private final JFileChooser chooser;
 
+    //Dialog zum Hinzufügen von Einträgen (Sched & App)
     private AddEntryDlg addingDialog;
+    //Dialog zur 'sicheren' Eingabe von Username und Password
     private LoginDlg loginDialog;
 
+    //Beinhaltet den Username in plaintext
     private String savedUsername;
+    //Inhalt des Passwords verschlüsselt
     private char[] savedPassword;
+    
 
+    //lokale gespeicherte Dateien, um dem User Arbeit zu ersparen
+    //wie savedUsername und Password
     private File savedLocalSchedulesFile;
     private File savedLocalAppointmentsFile;
 
@@ -40,6 +49,8 @@ public class GUI extends javax.swing.JFrame {
         listAppointments.setModel(appInv);
 
         chooser = new JFileChooser();
+        //im Ordner DataFiles befinden sich alle Daten die zur Laufzeit gesammelt wurden
+        //local Files
         chooser.setCurrentDirectory(new File("src/DataFiles"));
         chooser.setMultiSelectionEnabled(false); //nur zur Sicherheit
 
@@ -123,9 +134,19 @@ public class GUI extends javax.swing.JFrame {
         menuFile.add(onLoadFromRemote);
 
         onLoadFromLocal.setText("load from local");
+        onLoadFromLocal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                onLoadFromLocalActionPerformed(evt);
+            }
+        });
         menuFile.add(onLoadFromLocal);
 
         onSaveToLocal.setText("save to local");
+        onSaveToLocal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                onSaveToLocalActionPerformed(evt);
+            }
+        });
         menuFile.add(onSaveToLocal);
 
         onSaveToRemote.setText("save to remote");
@@ -138,31 +159,39 @@ public class GUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private int getCurrentTab(){
+        return panelSwitcher.getSelectedComponent().equals(plSchedules) ? SCHED_TAB : APP_TAB;
+    }
+    
     private void onAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onAddActionPerformed
         addingDialog = new AddEntryDlg(this, true);
-        int tab = -1;
-        if (panelSwitcher.getSelectedComponent().equals(plSchedules)) {
-            tab = 0;
+        
+        //gibt das gewählte Fenster an
+        int currentTab = getCurrentTab();
+        //true, wenn das momentane Fenster schedules anzeigt
+        if (currentTab == SCHED_TAB) {
+            //Default-Werte
             addingDialog.setTitle("Add schedule");
             addingDialog.setLabels("Subject:", "Until:");
             addingDialog.setTextFields("POS", "Serealisierung", LocalDate.now());
-        } else if (panelSwitcher.getSelectedComponent().equals(plAppointments)) {
-            tab = 1;
+        } else if (currentTab == APP_TAB) {
             addingDialog.setTitle("Add appointment");
             addingDialog.setLabels("Title:", "Date:");
             addingDialog.setTextFields("NVS", "Test", LocalDate.now());
         }
         addingDialog.setVisible(true);
 
+        
+        //wird nur ausgeführt, wenn etwas hinzugefügt werden soll
         if (addingDialog.wasOk()) {
-            if (tab == 0) {
+            if (currentTab == SCHED_TAB) {
                 try {
                     schedInv.add(new Schedule(addingDialog.getTitle(), addingDialog.getDescription(), addingDialog.getDate()));
                 } catch (DateTimeParseException dtpe) {
                     JOptionPane.showMessageDialog(this, dtpe.getMessage(),
                             "Fehler: Datum", JOptionPane.ERROR_MESSAGE);
                 }
-            } else if (tab == 1) {
+            } else if (currentTab == APP_TAB) {
                 try {
                     appInv.add(new Appointment(addingDialog.getTitle(), addingDialog.getDescription(), addingDialog.getDate()));
                 } catch (DateTimeParseException dtpe) {
@@ -171,10 +200,9 @@ public class GUI extends javax.swing.JFrame {
                 }
             }
         }
-
-
     }//GEN-LAST:event_onAddActionPerformed
 
+    //hauptsächlich fürs Ver- und Entschlüsseln gedacht
     private String charArrToString(char[] pass) {
         StringBuilder ret = new StringBuilder("");
         for (int i = 0; i < pass.length; i++) {
@@ -183,22 +211,21 @@ public class GUI extends javax.swing.JFrame {
         return ret.toString();
     }
 
-    private void onLoadFromRemoteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onLoadFromRemoteActionPerformed
-        String username = null;
-        String password = null;
+    private String[] getUsernameAndPasswordForRemoteAccess() {
+        //für die FTP-Verbindung
+        String username = null, plainPassword = null;
 
-        File localSchedulesFile = null;
-        File localAppointmentsFile = null;
-
-        int yesOrNo = -1;
+        //gibt an, ob die Logindaten gespeichert werden sollen
+        int yesOrNo;
 
         if (savedUsername != null && savedPassword != null) {
+            //wird nur ausgeführt, sollten schon Daten gespeichert worden sein
             if ((yesOrNo = JOptionPane.showConfirmDialog(this, "Use saved login?", "Login",
                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE))
-                    == JOptionPane.YES_OPTION) {
-
+                    == JOptionPane.YES_OPTION) 
+            {
                 username = savedUsername;
-                password = Cryptor.decrypt(savedPassword);
+                plainPassword = Cryptor.decrypt(savedPassword);
 
             } else if (yesOrNo == JOptionPane.NO_OPTION) {
                 loginDialog = new LoginDlg(this, true);
@@ -206,58 +233,45 @@ public class GUI extends javax.swing.JFrame {
                 loginDialog.setVisible(true);
 
                 username = loginDialog.getUsername();
-                password = charArrToString(loginDialog.getPassword());
-                loginDialog.clearFields();
+                plainPassword = charArrToString(loginDialog.getPassword());
+                loginDialog.clearFields(); //zur Sicherheit
 
                 if (loginDialog.getSaveLogin()) {
                     this.savedUsername = username;
-                    this.savedPassword = Cryptor.enrypt(password.toCharArray());
+                    this.savedPassword = Cryptor.enrypt(plainPassword.toCharArray());
                 }
             }
         } else if (savedUsername == null || savedPassword == null) {
             loginDialog = new LoginDlg(this, true);
-            loginDialog.setDefaults(savedUsername, Cryptor.decrypt(savedPassword));
+            loginDialog.setDefaults(DEFAULTUSERNAME, DEFAULTPASSWORD);
             loginDialog.setVisible(true);
 
             username = loginDialog.getUsername();
-            password = charArrToString(loginDialog.getPassword());
+            plainPassword = charArrToString(loginDialog.getPassword());
             loginDialog.clearFields();
 
             if (loginDialog.getSaveLogin()) {
                 this.savedUsername = username;
-                this.savedPassword = Cryptor.enrypt(password.toCharArray());
+                this.savedPassword = Cryptor.enrypt(plainPassword.toCharArray());
             }
         }
 
-        if (username != null && password != null) {
-            //DefaultFiles ? defFiles : chooser;
+        return new String[]{username, plainPassword};
+    }
 
-            yesOrNo = -1;
-            if (savedLocalSchedulesFile != null && savedLocalAppointmentsFile != null) {
-                if ((yesOrNo = JOptionPane.showConfirmDialog(this, "Use saved files?", "Files",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE))
-                        == JOptionPane.YES_OPTION) {
+    private File[] getLocalSchedulesAndAppointmentsFilesForSaving() {
+        File localSchedulesFile = null, localAppointmentsFile = null;
+        
+        int yesOrNo = -1;
+        if (savedLocalSchedulesFile != null && savedLocalAppointmentsFile != null) {
+            if ((yesOrNo = JOptionPane.showConfirmDialog(this, "Use saved files?", "Files",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE))
+                    == JOptionPane.YES_OPTION) {
 
-                    localSchedulesFile = savedLocalSchedulesFile;
-                    localAppointmentsFile = savedLocalAppointmentsFile;
+                localSchedulesFile = savedLocalSchedulesFile;
+                localAppointmentsFile = savedLocalAppointmentsFile;
 
-                } else if (yesOrNo == JOptionPane.NO_OPTION) {
-                    chooser.setDialogTitle("Local Schedules-File");
-                    localSchedulesFile = chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION ? chooser.getSelectedFile() : null;
-                    chooser.setDialogTitle("Local Appointments-File");
-                    localAppointmentsFile = chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION ? chooser.getSelectedFile() : null;
-
-                    if (JOptionPane.showConfirmDialog(this,
-                            "Set files as default-files to save to?",
-                            "Default Files", JOptionPane.YES_NO_OPTION,
-                            JOptionPane.PLAIN_MESSAGE) == JOptionPane.YES_OPTION) {
-
-                        savedLocalSchedulesFile = localSchedulesFile;
-                        savedLocalAppointmentsFile = localAppointmentsFile;
-
-                    }
-                }
-            } else if (savedLocalSchedulesFile == null || savedLocalAppointmentsFile == null) {
+            } else if (yesOrNo == JOptionPane.NO_OPTION) {
                 chooser.setDialogTitle("Local Schedules-File");
                 localSchedulesFile = chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION ? chooser.getSelectedFile() : null;
                 chooser.setDialogTitle("Local Appointments-File");
@@ -273,6 +287,42 @@ public class GUI extends javax.swing.JFrame {
 
                 }
             }
+        } else if (savedLocalSchedulesFile == null || savedLocalAppointmentsFile == null) {
+            chooser.setDialogTitle("Local Schedules-File");
+            localSchedulesFile = chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION ? chooser.getSelectedFile() : null;
+            chooser.setDialogTitle("Local Appointments-File");
+            localAppointmentsFile = chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION ? chooser.getSelectedFile() : null;
+
+            if (JOptionPane.showConfirmDialog(this,
+                    "Set files as default-files to save to?",
+                    "Default Files", JOptionPane.YES_NO_OPTION,
+                    JOptionPane.PLAIN_MESSAGE) == JOptionPane.YES_OPTION) {
+
+                savedLocalSchedulesFile = localSchedulesFile;
+                savedLocalAppointmentsFile = localAppointmentsFile;
+
+            }
+        }
+        
+        return new File[]{localSchedulesFile, localAppointmentsFile};
+    }
+
+    private void onLoadFromRemoteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onLoadFromRemoteActionPerformed
+        String username = null;
+        String password = null;
+
+        File localSchedulesFile = null;
+        File localAppointmentsFile = null;
+
+        String[] userAndPass = getUsernameAndPasswordForRemoteAccess();
+        username = userAndPass[0];
+        password = userAndPass[1];
+
+        if (username != null && password != null) {
+            //DefaultFiles ? defFiles : chooser;
+            File [] schedAndApp = getLocalSchedulesAndAppointmentsFilesForSaving();
+            localSchedulesFile = schedAndApp[0];
+            localAppointmentsFile = schedAndApp[1];
         }
 
         if (localSchedulesFile != null && localAppointmentsFile != null) {
@@ -313,6 +363,14 @@ public class GUI extends javax.swing.JFrame {
 
 
     }//GEN-LAST:event_onLoadFromRemoteActionPerformed
+
+    private void onLoadFromLocalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onLoadFromLocalActionPerformed
+
+    }//GEN-LAST:event_onLoadFromLocalActionPerformed
+
+    private void onSaveToLocalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onSaveToLocalActionPerformed
+        
+    }//GEN-LAST:event_onSaveToLocalActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -365,4 +423,14 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JScrollPane scrollAppointments;
     private javax.swing.JScrollPane scrollSchedules;
     // End of variables declaration//GEN-END:variables
+
+
+    
+    //Konstanten
+    public static final String DEFAULTUSERNAME = "anonymous";
+    public static final String DEFAULTPASSWORD = "";
+    
+    private static final int SCHED_TAB = 0;
+    private static final int APP_TAB = 1;
+
 }
