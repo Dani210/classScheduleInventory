07b99454d960d619daa5d54a25b3f4084e78ee93
owner <owner@breath>
2018-10-23 22:00:29 +0200

    Cryptor funzt

--- Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
+++ Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
@@ -4,28 +4,30 @@ public class Cryptor {
 
     public static char[] enrypt(char[] plainPassword) {
         if (plainPassword != null) {
-            char[] encrypted = new char[0];
-            for (int i = 0; i < plainPassword.length-1; i++) {
-                encrypted[i] = (char) ((double) plainPassword[i]
-                        * Math.sin((double) plainPassword[i]));
+            if (plainPassword.length > 0) {
+                char[] encrypted = new char [plainPassword.length];
+                for (int i = 0; i < plainPassword.length - 1; i++) {
+                    encrypted[i] = (char) ((double) plainPassword[i]
+                            * Math.sin((double) plainPassword[i]));
+                }
+                return encrypted;
             }
-            return encrypted;
         }
-
         return new char[10];
-        
     }
 
     public static String decrypt(char[] encryptedPass) {
         if (encryptedPass != null) {
-            String plain = "";
-            for (int i = 0; i < encryptedPass.length-1; i++) {
-                plain += (char) ((double) encryptedPass[i])
-                        / Math.sin((double) encryptedPass[i]);
+            if (encryptedPass.length > 0) {
+                String plain = "";
+                for (int i = 0; i < encryptedPass.length - 1; i++) {
+                    plain += (char) ((double) encryptedPass[i])
+                            / Math.sin((double) encryptedPass[i]);
+                }
+                return plain;
             }
-            return plain;
-        }
 
+        }
         return new String("Password");
     }
 }
