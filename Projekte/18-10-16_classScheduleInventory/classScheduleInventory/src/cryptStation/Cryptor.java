a1272df81bec4a6bfe5b68dcefdc5e8604aae88d
owner <owner@breath>
2018-10-24 21:09:10 +0200

    CreateKey funktioniert

--- Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
+++ Projekte/18-10-16_classScheduleInventory/classScheduleInventory/src/cryptStation/Cryptor.java
@@ -1,33 +1,65 @@
 package cryptStation;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
+
 public class Cryptor {
+    
+    private static final int DEFAULT_HASH_LENGTH = 256;
+    
+    
+    //256 Zeichen groß (immer)
+    private static char [] temporalKey;
 
     public static char[] enrypt(char[] plainPassword) {
-        if (plainPassword != null) {
-            if (plainPassword.length > 0) {
-                char[] encrypted = new char [plainPassword.length];
-                for (int i = 0; i < plainPassword.length - 1; i++) {
-                    encrypted[i] = (char) ((double) plainPassword[i]
-                            * Math.sin((double) plainPassword[i]));
-                }
-                return encrypted;
-            }
+        if(Cryptor.temporalKey == null){
+            //createKey
+            createKey(DEFAULT_HASH_LENGTH);
         }
+        else{
+            //encrypt with key
+        }
+        
         return new char[10];
     }
 
     public static String decrypt(char[] encryptedPass) {
-        if (encryptedPass != null) {
-            if (encryptedPass.length > 0) {
-                String plain = "";
-                for (int i = 0; i < encryptedPass.length - 1; i++) {
-                    plain += (char) ((double) encryptedPass[i])
-                            / Math.sin((double) encryptedPass[i]);
-                }
-                return plain;
-            }
-
-        }
-        return new String("Password");
+        //decrypt with key
+        return "";
     }
+    
+    private static void createKey(int hashLength){
+        StringBuilder hashD = new StringBuilder("" + LocalDate.now().hashCode());
+        StringBuilder hashT = new StringBuilder("" + LocalTime.now().hashCode());
+        Random randy = new Random();
+        
+        StringBuilder finalHash = new StringBuilder();
+        
+        //hashD -> leading zeroes
+        //offset == Stelle an der eingefügt wird
+        while(hashD.length() < hashLength){
+            //offset == Stelle an der eingefügt wird
+            hashD = hashD.insert(0, (char) randy.nextInt());
+        }
+        
+        //hashT -> trailing zeroes
+        while(hashT.length() < hashLength){
+            //offset == Stelle an der eingefügt wird
+            hashT = hashT.insert(hashT.length(), (char) randy.nextInt());
+        }
+        
+        
+        //anded -> hashD & hashT
+        for(int i = 0; i < hashLength; i++){
+            finalHash.append((char)
+                    (((int)hashD.charAt(i)) ^ ((int)hashT.charAt(i)))
+            );
+        }
+    }
+    
+    public static void main(String[] args) {
+        createKey(DEFAULT_HASH_LENGTH);
+    }
+    
 }
